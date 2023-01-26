b2b484f242ee31eb4c021d4d9a8c15d913c111bc
sahuh <tailikunal08@gmail.com>
2023-01-26 10:27:31 +0530

    hy

---
+++ android/app/src/main/kotlin/com/example/color_penal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.color_penal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:color_penal/next.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
